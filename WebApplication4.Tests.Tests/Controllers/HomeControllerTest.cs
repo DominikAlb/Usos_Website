6a04643ff653d54765d0311097a3cb1f262a7139
Dominik Albiniak <dominik.albiniak@student.uj.edu.pl>
2019-01-18 01:16:53 +0100

    tests and more

---
+++ WebApplication4.Tests.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication4;
+using WebApplication4.Controllers;
+
+namespace WebApplication4.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+
+            HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Index() as ViewResult;
+            var result = controller.Session["Exams"];
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CallendarIsEmpty()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            var result = controller.Session["Login"];
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void SearchForCourse()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.SearchForCourse() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
